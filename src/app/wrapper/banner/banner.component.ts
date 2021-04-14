73f3dceba31573be8932050ba5bd7209af85a205
Luc <lucborris@gmail.com>
2021-04-14 14:47:49 -0400

    Add font awesome to banner component

--- src/app/wrapper/banner/banner.component.ts
+++ src/app/wrapper/banner/banner.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { faGem } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
+  faGem = faGem;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
