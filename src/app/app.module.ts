b9f550719ea449db03f768df73205891b5bf419a
Luc <lucborris@gmail.com>
2021-04-15 12:52:48 -0400

    Add project templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { BannerComponent } from './wrapper/banner/banner.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProjectsComponent } from './wrapper/projects/projects.component';
+import { ProjectItemComponent } from './wrapper/projects/project-item/project-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     WrapperComponent,
-    BannerComponent
+    BannerComponent,
+    ProjectsComponent,
+    ProjectItemComponent
   ],
   imports: [
     BrowserModule,
