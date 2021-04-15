2fc7c41217405889230523dc4a067b71739d848f
Luc <lucborris@gmail.com>
2021-04-15 12:34:15 -0400

    Add fade in for background image

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss'],
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent implements AfterViewInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  @ViewChild('header', { static: true }) header: ElementRef;
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.header.nativeElement.classList.remove('is-loading');
+    }, 100);
+  }
 }
