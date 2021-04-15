aae7878f987e55f6f3bf299e76e3cd7fc8ba1f23
Luc <lucborris@gmail.com>
2021-04-15 14:10:43 -0400

    Add projects mock data for ngFor

--- src/app/wrapper/projects/projects.component.ts
+++ src/app/wrapper/projects/projects.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { projects } from './projects';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects: { id: number; name: string; description: string }[] = projects;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
