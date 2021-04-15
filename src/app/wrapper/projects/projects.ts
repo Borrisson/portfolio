3d4ef778ed9dec85ae9b98f2e121726657d71dc9
Luc <lucborris@gmail.com>
2021-04-15 13:30:03 -0400

    Add mock data for projects

---
+++ src/app/wrapper/projects/projects.ts
@@ -0,0 +1,8 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'HandLoose',
+    description:
+      'This application will help individuals increase their keyboard typing skill whilst playing a game.',
+  },
+];
