b7afe725e3df9ab75f818d7718ce451f59d96be7
Luc <lucborris@gmail.com>
2021-04-15 14:24:20 -0400

    Add project input into child componenet

--- src/app/wrapper/projects/project-item/project-item.component.ts
+++ src/app/wrapper/projects/project-item/project-item.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-item',
   templateUrl: './project-item.component.html',
-  styleUrls: ['./project-item.component.scss']
+  styleUrls: ['./project-item.component.scss'],
 })
-export class ProjectItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProjectItemComponent {
+  @Input() project: { id: number; name: string; description: string };
 }
