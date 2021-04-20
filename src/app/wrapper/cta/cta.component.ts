8510abc1572aa3db19feff7af6559cd1f86c0957
lucborris <lucborris@gmail.com>
2021-04-20 10:47:22 -0400

    Refactor icons to use ngfor

--- src/app/wrapper/cta/cta.component.ts
+++ src/app/wrapper/cta/cta.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cta.component.scss'],
 })
 export class CtaComponent implements OnInit {
+  languageIcons: string[];
+  frameworkIcons: string[];
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.languageIcons = [
+      'devicon-javascript-plain',
+      'devicon-css3-plain-wordmark',
+      'devicon-html5-plain-wordmark',
+      'devicon-ruby-plain',
+      'devicon-sass-original',
+      'devicon-postgresql-plain',
+    ];
+    this.frameworkIcons = [
+      'devicon-nodejs-plain',
+      'devicon-jquery-plain ',
+      'devicon-angularjs-plain',
+      'devicon-react-original',
+      'devicon-rails-plain',
+      'devicon-redis-plain',
+      'devicon-bootstrap-plain',
+    ];
+  }
 }
