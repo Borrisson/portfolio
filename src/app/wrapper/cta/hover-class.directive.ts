bcbc7cdc708f1efdd3f007744b7d6affed666c2b
lucborris <lucborris@gmail.com>
2021-04-20 10:14:31 -0400

    Add hover class

--- src/app/wrapper/cta/hover-class.directive.ts
+++ src/app/wrapper/cta/hover-class.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHoverClass]'
+  selector: '[appHoverClass]',
 })
 export class HoverClassDirective {
+  constructor(public el: ElementRef) {}
+  @Input('appHoverClass') hoverClass: string;
 
-  constructor() { }
+  @HostListener('mouseenter') onMouseEnter() {
+    console.log(this.hoverClass);
+    this.el.nativeElement.classList.add(this.hoverClass);
+  }
 
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.classList.remove(this.hoverClass);
+  }
 }
