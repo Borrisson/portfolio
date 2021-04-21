6337ddfbfc04d9f9ff06e034852d872805e57c97
lucborris <lucborris@gmail.com>
2021-04-21 11:58:50 -0300

    Remove icon styling from footer

--- src/app/wrapper/footer/footer.component.ts
+++ src/app/wrapper/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { faIdCard } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +6,5 @@ import { faIdCard } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  faIdCard = faIdCard;
   constructor() {}
 }
