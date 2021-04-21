351c085c9ec4ffaec476421431e471a9239680be
lucborris <lucborris@gmail.com>
2021-04-21 11:41:07 -0300

    Add icon to footer for contact info

--- src/app/wrapper/footer/footer.component.ts
+++ src/app/wrapper/footer/footer.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { faIdCard } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  faIdCard = faIdCard;
   constructor() {}
-
-  ngOnInit(): void {}
 }
