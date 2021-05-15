56d4f8d11a6248936f3cdb9378a65361e3d723e7
lucborris <lucborris@gmail.com>
2021-05-15 10:11:12 -0300

    Add http client for contact ajax

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
