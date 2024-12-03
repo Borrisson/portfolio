78c9c87b5525724d7cb25c3e05abf9961a5fe8c2
BORRISSON <lucborris@gmail.com>
2024-12-03 15:11:12 -0800

    update ts file

--- src/app/wrapper/cta/cta.component.ts
+++ src/app/wrapper/cta/cta.component.ts
@@ -1,41 +1,63 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-cta',
-    templateUrl: './cta.component.html',
-    styleUrls: ['./cta.component.scss'],
-    standalone: false
+  selector: 'app-cta',
+  templateUrl: './cta.component.html',
+  styleUrls: ['./cta.component.scss'],
+  standalone: false,
 })
 export class CtaComponent {
-  languageIcons: string[];
-  frameworkIcons: string[];
-  databaseIcons: string[];
-  constructor() {
-    this.languageIcons = [
-      'javascript-plain',
-      'typescript-plain',
-      'css3-plain',
-      'html5-plain',
-      'ruby-plain',
-      'sass-original',
-      'groovy-plain',
-    ];
-    this.frameworkIcons = [
-      'nodejs-plain',
-      'jquery-plain ',
-      'angularjs-plain',
-      'react-original',
-      'rails-plain',
-      'redis-plain',
-      'bootstrap-plain',
-      'materialui-plain',
-      'express-original',
-      'grails-plain',
-      'nextjs-plain',
-      'redux-original',
-    ];
-    this.databaseIcons = ['mongodb-plain', 'postgresql-plain'];
-  }
+  private languageIcons = [
+    'javascript-plain',
+    'typescript-plain',
+    'css3-plain',
+    'html5-plain',
+    'ruby-plain',
+    'sass-original',
+    'groovy-plain',
+    'kotlin-plain',
+    'java-plain',
+    'csharp-plain',
+  ];
+  private frameworkIcons = [
+    'nodejs-plain',
+    'jquery-plain ',
+    'angularjs-plain',
+    'react-original',
+    'rails-plain',
+    'redis-plain',
+    'bootstrap-plain',
+    'materialui-plain',
+    'express-original',
+    'grails-plain',
+    'nextjs-plain',
+    'redux-original',
+    'dotnetcore-plain',
+    'ktor-plain',
+  ];
+  private databaseIcons = ['mongodb-plain', 'postgresql-plain'];
+
+  private cloudProviderIcons = ['amazonwebservices-plain', 'googlecloud-plain'];
+
+  skills = [
+    {
+      name: 'Languages',
+      icons: this.languageIcons,
+    },
+    {
+      name: 'Frameworks',
+      icons: this.frameworkIcons,
+    },
+    {
+      name: 'Databases',
+      icons: this.databaseIcons,
+    },
+    {
+      name: 'Cloud Providers',
+      icons: this.cloudProviderIcons,
+    },
+  ];
+
   filterNameOnly(classname: string): string {
     return (
       classname.charAt(0).toUpperCase() +
